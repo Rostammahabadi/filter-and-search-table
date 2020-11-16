a0759f74c0de32763fc0b333cb7c835ef25c63e9
Rostam Mahabadi <RostamMahabadi@gmail.com>
2020-11-16 13:17:53 -0700

    set state and added sorting function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { sort } from './utils/index.js'
 import "./style.css";
 
 function App() {
+  const [state, setState] = useState({
+    data: [],
+  });
 
+  useEffect(() => {
+    fetch("https://code-challenge.spectrumtoolbox.com/api/restaurants", {
+      headers: {
+        Authorization: process.env.REACT_APP_API_KEY
+      }
+    })
+    .then(response => response.json())
+    .then(json => 
+      setState(prev => ({
+        ...prev,
+        data: sort(json),
+      })))
+    
+  }, []);
   return (
     <div>
 
---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+function sort(response) {
+  return response.sort(function(a, b) {
+    var nameA = a.name.toUpperCase();
+    var nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+}
