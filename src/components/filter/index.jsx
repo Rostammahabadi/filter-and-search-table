fdbb0fc8189463ba5bea8ed5dea57f332d8da0e6
Rostam Mahabadi <RostamMahabadi@gmail.com>
2020-11-20 10:18:15 -0700

    added class name for testing

--- src/components/filter/index.jsx
+++ src/components/filter/index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 export default function Filter({ data, onChange }) {
     
     return (
-        <select style={{width:100}}onChange={(e) => onChange(e.target.value, e)}>
+        <select style={{width:100}}onChange={(e) => onChange(e.target.value, e)} className={'filter'}>
             <option></option>
             {data.map( content => (
                 <option key={content} value={content}>{content}</option>
